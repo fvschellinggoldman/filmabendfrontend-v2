4b8c0cb586d16a56d5468b0edbe90719c77731f2
Fabio von Schelling Goldman <faar@netlight.com>
2024-07-15 19:12:32 +0200

    WIP

--- src/components/Statistics/PersonalStatisticsContainer.tsx
+++ src/components/Statistics/PersonalStatisticsContainer.tsx
@@ -2,8 +2,11 @@ import React, { FC } from "react";
 import { useFetchPersonalStatistics } from "../../api/users/PersonalStatistics";
 import { User } from "../../types/user";
 import { PersonalStatisticCard } from "./PersonalStatisticCard";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import styles from "./PersonalStatisticsCardContainer.module.scss";
+import { isMobile } from "react-device-detect";
 
 interface PersonalStatisticsContainerProps {
   user: User;
@@ -18,7 +21,27 @@ export const PersonalStatisticsContainer: FC<
     return null;
   }
 
-  return (
+  return isMobile ? (
+    <div className={styles.MobilePersonalStatisticCardContainer}>
+      <Slider
+        infinite={true}
+        centerPadding="60px"
+        className="center"
+        swipeToSlide={true}
+        slidesToShow={1}
+        slidesToScroll={1}
+      >
+        {statistics.map((statistic) => (
+          <div className={styles.PersonalStatisticCard} key={statistic.title}>
+            <PersonalStatisticCard
+              title={statistic.title}
+              content={statistic.content}
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  ) : (
     <div className={styles.PersonalStatisticCardContainer}>
       {statistics.map((statistic) => (
         <div className={styles.PersonalStatisticCard} key={statistic.title}>
