49cb0980e39d87ed20f145c1b4ec7de132c90045
Fabio von Schelling Goldman <faar@netlight.com>
2024-08-18 09:25:31 +0200

    small fix

--- src/api/movies/Movies.ts
+++ src/api/movies/Movies.ts
@@ -1,72 +1,67 @@
 import useSWR, { mutate } from "swr";
-import {  MovieDetail, MovieSearchResult, MovieSuggestion } from "../../types/movie";
+import {
+  MovieDetail,
+  MovieSearchResult,
+  MovieSuggestion,
+} from "../../types/movie";
 import { getRequest, postRequest, putRequest } from "../api";
-  
- async function _addMovie(
-    movieToAdd: MovieSearchResult | MovieSuggestion,
-    eventId: number
-  ) {
-    const url = "/api/movie";
-  
-    await postRequest(url, {
-      title: movieToAdd.title,
-      releaseDate: movieToAdd.releaseDate,
-      tmdbId: movieToAdd.tmdbId,
-      eventId: eventId,
-    });
-  }
 
-  export function addSearchedMovie(
-    searchedMovie: MovieSearchResult,
-    eventId: number
-  ) {
-    _addMovie(searchedMovie, eventId)
-    mutate("/api/event");
-  }
-  
-  export function addSuggestedMovie(
-    suggestedMovie?: MovieSuggestion,
-  ) {
-    if (suggestedMovie){
-      _addMovie(
-        suggestedMovie,
-        suggestedMovie.eventId
-      )
-      _updateSuggestionState(
-        suggestedMovie,
-        "ACCEPTED"
-      )
-    }
-  }
+async function _addMovie(
+  movieToAdd: MovieSearchResult | MovieSuggestion,
+  eventId: number
+) {
+  const url = "/api/movie";
 
-  export function declineSuggestedMovie(
-    suggestedMovie?: MovieSuggestion
-  ) {
-    if(suggestedMovie) _updateSuggestionState(suggestedMovie, 'DECLINED')
-  }
-  
-  export function useFetchMovie(movieId: number) {
-    const api_url = `/api/movie/${movieId}`;
+  await postRequest(url, {
+    title: movieToAdd.title,
+    releaseDate: movieToAdd.releaseDate,
+    tmdbId: movieToAdd.tmdbId,
+    eventId: eventId,
+  });
+}
 
-    const { data, error, isLoading } = useSWR<MovieDetail, Error>(
-      api_url,
-      (url) => getRequest(url, null),
-      { revalidateOnFocus: false }
-    );
-  
-    return {
-      movie: data,
-      isLoading,
-      isError: error,
-    };
-  }
+export function addSearchedMovie(
+  searchedMovie: MovieSearchResult,
+  eventId: number
+) {
+  _addMovie(searchedMovie, eventId);
+  mutate("/api/event");
+}
 
- async function _updateSuggestionState(
-    suggestedMovie: MovieSuggestion, newState: string
-  ) {
-    const url = `/api/event/${suggestedMovie.eventId}/${suggestedMovie.id}/update_suggestion_state`;
-    await putRequest(url, {
-      newState
-    });
-    mutate(`/api/event/${suggestedMovie.eventId}/suggestion`);
+export function addSuggestedMovie(suggestedMovie?: MovieSuggestion) {
+  if (suggestedMovie) {
+    _addMovie(suggestedMovie, suggestedMovie.eventId);
+    _updateSuggestionState(suggestedMovie, "ACCEPTED");
   }
+}
+
+export function declineSuggestedMovie(suggestedMovie?: MovieSuggestion) {
+  if (suggestedMovie) _updateSuggestionState(suggestedMovie, "DECLINED");
+}
+
+export function useFetchMovie(movieId: number) {
+  const api_url = `/api/movie/${movieId}`;
+
+  const { data, error, isLoading } = useSWR<MovieDetail, Error>(
+    api_url,
+    (url) => getRequest(url, null),
+    { revalidateOnFocus: false }
+  );
+
+  return {
+    movie: data,
+    isLoading,
+    isError: error,
+  };
+}
+
+async function _updateSuggestionState(
+  suggestedMovie: MovieSuggestion,
+  newState: string
+) {
+  const url = `/api/event/${suggestedMovie.eventId}/${suggestedMovie.id}/update_suggestion_state`;
+  await putRequest(url, {
+    newState,
+  });
+  mutate(`/api/event/${suggestedMovie.eventId}/suggestion`);
+}
